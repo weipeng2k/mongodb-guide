9b5a91c979bde1996d106b7afb2d21de71778536
weipeng2k <weipeng2k@126.com>
2019-01-01 17:46:07 +0800

    feat(mg): first commit.

---
+++ src/main/java/com/murdock/books/mongodbguide/MongodbGuideApplication.java
@@ -0,0 +1,14 @@
+package com.murdock.books.mongodbguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbGuideApplication.class, args);
+	}
+
+}
+
