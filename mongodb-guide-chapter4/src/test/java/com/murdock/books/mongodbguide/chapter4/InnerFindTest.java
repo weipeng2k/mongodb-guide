68a2956d0bae6bffdccf1a9f7aec95ed0011be45
weipeng2k <weipeng2k@126.com>
2019-12-08 21:58:46 +0800

    feat(mongdb-guide-chapter4): 除游标外的内容，示例

---
+++ mongodb-guide-chapter4/src/test/java/com/murdock/books/mongodbguide/chapter4/InnerFindTest.java
@@ -0,0 +1,68 @@
+package com.murdock.books.mongodbguide.chapter4;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.murdock.books.mongodbguide.common.config.MongoConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author weipeng2k 2019年11月03日 下午16:43:41
+ */
+@SpringBootTest(classes = InnerFindTest.Config.class)
+@RunWith(SpringRunner.class)
+@TestPropertySource("classpath:application-test.properties")
+public class InnerFindTest {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void all() {
+        DBCollection collection = mongoTemplate.getCollection("foo");
+
+        DBObject query = new BasicDBObject();
+
+        DBObject condition = new BasicDBObject();
+        condition.put("$all", new int[]{1, 2});
+
+        query.put("value", condition);
+
+        System.out.println(query);
+
+        DBCursor dbObjects = collection.find(query);
+        dbObjects.forEach(System.out::println);
+    }
+
+    @Test
+    public void size() {
+        DBCollection collection = mongoTemplate.getCollection("foo");
+
+        DBObject query = new BasicDBObject();
+
+        DBObject condition = new BasicDBObject();
+        condition.put("$size", 3);
+
+        query.put("value", condition);
+
+        System.out.println(query);
+
+        DBCursor dbObjects = collection.find(query);
+        dbObjects.forEach(System.out::println);
+    }
+
+    @Configuration
+    @Import(MongoConfig.class)
+    static class Config {
+
+    }
+}
